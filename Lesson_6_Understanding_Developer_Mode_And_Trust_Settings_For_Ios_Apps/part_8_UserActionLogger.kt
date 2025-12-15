b9aa7ef17816458c207590d5b1749b86ccee9a5a
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-15 02:55:52 -0500

    Add Lesson_6_Understanding_Developer_Mode_And_Trust_Settings_For_Ios_Apps/part_8_UserActionLogger.kt

---
+++ Lesson_6_Understanding_Developer_Mode_And_Trust_Settings_For_Ios_Apps/part_8_UserActionLogger.kt
@@ -0,0 +1,8 @@
+import android.util.Log
+
+fun logUserAction(action: String) {
+    Log.d("UserAction", action)
+}
+
+// Call this function whenever the user performs an action
+logUserAction("Button clicked")
