43090a9fed8299566c42c7520d8ed2f6d373f1fe
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-15 02:55:10 -0500

    Add Lesson_28_Understanding_Swift_Programming_Concepts_For_Ios_Development/part_8_grade_feedback.swift

---
+++ Lesson_28_Understanding_Swift_Programming_Concepts_For_Ios_Development/part_8_grade_feedback.swift
@@ -0,0 +1,12 @@
+let grade = "B"
+
+switch grade {
+case "A":
+    print("Excellent work!")
+case "B":
+    print("Good job!")
+case "C":
+    print("You can do better!")
+default:
+    print("Keep trying!")
+}
