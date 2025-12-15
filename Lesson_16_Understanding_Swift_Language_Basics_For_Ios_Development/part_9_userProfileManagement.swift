313dc9a942d888c28d4e1ac9858e187003c69c04
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-15 02:53:56 -0500

    Add Lesson_16_Understanding_Swift_Language_Basics_For_Ios_Development/part_9_userProfileManagement.swift

---
+++ Lesson_16_Understanding_Swift_Language_Basics_For_Ios_Development/part_9_userProfileManagement.swift
@@ -0,0 +1,3 @@
+var userProfile: [String: String] = ["name": "John Doe", "email": "johndoe@example.com"]
+print(userProfile["name"]!) // Outputs "John Doe"
+userProfile["age"] = "30" // Adds a new key-value pair
