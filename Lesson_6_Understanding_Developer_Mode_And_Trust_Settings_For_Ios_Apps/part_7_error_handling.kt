a8b1fbde0c2423262f832c905600ca5669483ccd
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-15 02:55:50 -0500

    Add Lesson_6_Understanding_Developer_Mode_And_Trust_Settings_For_Ios_Apps/part_7_error_handling.kt

---
+++ Lesson_6_Understanding_Developer_Mode_And_Trust_Settings_For_Ios_Apps/part_7_error_handling.kt
@@ -0,0 +1,7 @@
+try {
+    // Code that may throw an exception
+    val result = potentiallyRiskyOperation()
+} catch (e: Exception) {
+    // Handle the error appropriately
+    println("An error occurred: ${e.message}")
+}
