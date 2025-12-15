819edfb892efa8c04d2d2b1916f1d19231981d25
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-15 02:55:03 -0500

    Add Lesson_28_Understanding_Swift_Programming_Concepts_For_Ios_Development/part_3_variable_examples.swift

---
+++ Lesson_28_Understanding_Swift_Programming_Concepts_For_Ios_Development/part_3_variable_examples.swift
@@ -0,0 +1,5 @@
+var score: Int = 100
+// score = "A perfect score" // This will cause a compilation error
+var fruits: [String] = ["Apple", "Banana", "Cherry"]
+fruits.append("Orange")
+print(fruits) // This will print: ["Apple", "Banana", "Cherry", "Orange"]
