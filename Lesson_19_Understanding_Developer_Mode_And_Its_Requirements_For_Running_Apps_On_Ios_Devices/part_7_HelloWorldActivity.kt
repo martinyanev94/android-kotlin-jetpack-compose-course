1b1a17d663f6d28676cabe16a8a3a3107e004eeb
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-15 02:54:17 -0500

    Add Lesson_19_Understanding_Developer_Mode_And_Its_Requirements_For_Running_Apps_On_Ios_Devices/part_7_HelloWorldActivity.kt

---
+++ Lesson_19_Understanding_Developer_Mode_And_Its_Requirements_For_Running_Apps_On_Ios_Devices/part_7_HelloWorldActivity.kt
@@ -0,0 +1,7 @@
+override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textView)
+        textView.text = "Hello, World!" // Set a breakpoint here
+    }
