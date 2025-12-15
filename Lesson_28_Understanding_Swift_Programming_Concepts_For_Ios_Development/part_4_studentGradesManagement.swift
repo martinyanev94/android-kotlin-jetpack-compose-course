963b315bf050fcc5c47cb2b068ababf8c78e7722
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-15 02:55:04 -0500

    Add Lesson_28_Understanding_Swift_Programming_Concepts_For_Ios_Development/part_4_studentGradesManagement.swift

---
+++ Lesson_28_Understanding_Swift_Programming_Concepts_For_Ios_Development/part_4_studentGradesManagement.swift
@@ -0,0 +1,3 @@
+var studentGrades: [String: String] = ["Alice": "A", "Bob": "B", "Charlie": "C"]
+studentGrades["Diana"] = "A"
+print(studentGrades) // This will print: ["Alice": "A", "Bob": "B", "Charlie": "C", "Diana": "A"]
