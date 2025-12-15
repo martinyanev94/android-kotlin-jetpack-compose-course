70f0c335405c55ba6d0fea0ae63c319ff6ecb89a
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-15 02:55:40 -0500

    Add Lesson_4_Developing_And_Running_Your_Ios_App_On_A_Physical_Device/part_4_android_build_config.kt

---
+++ Lesson_4_Developing_And_Running_Your_Ios_App_On_A_Physical_Device/part_4_android_build_config.kt
@@ -0,0 +1,10 @@
+android {
+    compileSdkVersion(30)
+    defaultConfig {
+        applicationId "com.yourdomain.yourapp"
+        minSdkVersion(21)
+        targetSdkVersion(30)
+        versionCode 1
+        versionName "1.0"
+    }
+}
