9ea49b7bbcb79e61787d14d966e808b1136bd408
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-15 02:54:11 -0500

    Add Lesson_18_Understanding_Swift_Language_Basics_For_Ios_Development/part_6_calculate_area.swift

---
+++ Lesson_18_Understanding_Swift_Language_Basics_For_Ios_Development/part_6_calculate_area.swift
@@ -0,0 +1,18 @@
+func calculateArea(width: Double, height: Double) -> Double {
+    return width * height
+}
+let area = calculateArea(width: 5.0, height: 10.0)
+print("The area is \(area)")
+
+swift
+func calculateArea(width: Double, height: Double) -> Double {
+    return width * height
+}
+
+swift
+let area = calculateArea(width: 5.0, height: 10.0)
+print("The area is \(area)")
+
+
+The area is 50.0
+
